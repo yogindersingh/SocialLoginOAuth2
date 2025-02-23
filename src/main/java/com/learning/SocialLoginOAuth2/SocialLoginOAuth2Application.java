7c5937d4f1a000fa690ea6110bb4f86d208b5c45
yogindersingh <yogindersingh@fico.com>
2025-02-23 19:34:33 +0530

    Code changes for social login using github

---
+++ src/main/java/com/learning/SocialLoginOAuth2/SocialLoginOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.learning.SocialLoginOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginOAuth2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/learning/SocialLoginOAuth2/configuration/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.learning.SocialLoginOAuth2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests(req->req.requestMatchers("/secure").authenticated().anyRequest().permitAll())
+        .formLogin(Customizer.withDefaults())
+        .oauth2Login(Customizer.withDefaults())
+        .build();
+  }
+
+  @Bean
+  ClientRegistrationRepository clientRegistrationRepository() {
+    ClientRegistration github = githubClientRegistration();
+    return new InMemoryClientRegistrationRepository(github);
+  }
+
+  private ClientRegistration githubClientRegistration() {
+    return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("Ov23liaTteCYtwt1EpEJ")
+        .clientSecret("315bb4803c091b85dba242cc3b32637c53dace84").build();
+  }
+}
